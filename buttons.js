4d348f4a798f2a4135bd2dad2b8710489f8a6c7b
vbreut <vincent.breut@gmail.com>
2025-06-13 14:00:40 +0200

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -67,6 +67,7 @@ function showPage() {
         if (pseudo !== "" && pseudo!==null){
             let joueurRef = database.ref('joueurs/' + pseudo);
             joueurRef.set({ enLigne: "en partie"});
+            joueurRef.onDisconnect().remove();
         }
 
         if(playingmode==1){
