ab7074848c6aa7dca480810be515a976cea74ca5
vbreut <vincent.breut@gmail.com>
2025-06-16 14:22:11 +0200

    Update online.js

--- online.js
+++ online.js
@@ -112,6 +112,7 @@ function waitforconnected(joueurRef){
     });
 }
 
+sauvpseudo.addEventListener('click',sauvegarderPseudo);
 
 function sauvegarderPseudo(){
     pseudo = document.getElementById("pseudoInput").value.trim();
