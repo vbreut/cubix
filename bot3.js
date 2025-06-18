990e1b14b10ea2fe244a471ca649215a93344467
vbreut <vincent.breut@gmail.com>
2025-06-18 07:07:58 +0200

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -125,7 +125,7 @@ function pathtovictory(deepmax){
             }
         }*/
 
-        for(i=0;i<5;i++){
+        for(i=0;i<20;i++){
             taille[i]=i;
         }
 
@@ -430,7 +430,7 @@ function menace(deepmax){
     if(found[0]>0){
         //recherche du plus court. On peut louper une menace d'égale longueur... tant pis.
 
-        for(i=0;i<5;i++){
+        for(i=0;i<20;i++){
             taille[i]=i;
         }
 
